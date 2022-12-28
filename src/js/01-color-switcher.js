30450419f42bde1e90959edfe1839909664f12fc
Koverin <110396064+Koverin@users.noreply.github.com>
2022-12-28 15:51:45 +0200

    error

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+// const startBtn = document.querySelector('button[data-start]');
+console.log('ewqeqewqe');
 
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// getRandomHexColor();
+
+// const button = document.querySelector('.change-color');
+// const color = document.querySelector('.color');
+
+// button.addEventListener('click', () => {
+//   const hexColor = getRandomHexColor();
+//   document.body.style.backgroundColor = hexColor;
+//   color.textContent = hexColor;
+
+//   console.log('click');
+//   console.log(hexColor);
+// });
+
+// // getRandomHexColor();
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
