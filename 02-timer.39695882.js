fe17c448ec96caf0a350b368b4b982db8ccd0745
Koverin <110396064+Koverin@users.noreply.github.com>
2023-01-04 17:26:32 +0000

    Deploying to gh-pages from @ Koverin/goit-js-hw-09@96529170786d974189d05e5f8470f2c7f5aa3e13 🚀

--- 02-timer.39695882.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".timer");console.log(e);
-//# sourceMappingURL=02-timer.39695882.js.map
--- 02-timer.ed7e3c80.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".timer");console.log(e)}();
-//# sourceMappingURL=02-timer.ed7e3c80.js.map
